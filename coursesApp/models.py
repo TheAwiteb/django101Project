a3f16d8ebdb4512ee55f1d1634bd12506114f659
TheAwiteb <59842932+TheAwiteb@users.noreply.github.com>
2021-09-23 10:48:21 +0300

    add course number range

--- coursesApp/models.py
+++ coursesApp/models.py
@@ -1,10 +1,11 @@
 from django.db import models
 from django.db.models.fields import PositiveSmallIntegerField
+from django.core.validators import MaxValueValidator, MinValueValidator 
 
 
 class Courses(models.Model):
     name = models.CharField(max_length=100, null=False, help_text="Name of the course.")
-    number = PositiveSmallIntegerField(null=False, help_text="Number of the course.")
+    number = PositiveSmallIntegerField(null=False, validators=[MinValueValidator(101), MaxValueValidator(10000)], help_text="Number of the course.")
     timestamp = models.DateTimeField(
         auto_now_add=True, help_text="Date of adding the course."
     )
