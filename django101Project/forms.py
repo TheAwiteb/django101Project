fbfd2f3e991f977efa26df7651714be972840737
TheAwiteb <59842932+TheAwiteb@users.noreply.github.com>
2021-10-01 21:21:54 +0300

    Possibility to change the password

--- django101Project/forms.py
+++ django101Project/forms.py
@@ -2,6 +2,9 @@ from django import forms
 from django.contrib.auth.models import User
 from registration.models import Profile
 
+from django.contrib.auth import password_validation
+from django.core.exceptions import ValidationError
+
 
 class UserUpdateForm(forms.ModelForm):
     username = forms.CharField(
@@ -34,3 +37,55 @@ class ProfileUpdateForm(forms.ModelForm):
     class Meta:
         model = Profile
         fields = ["image", "bio"]
+
+
+class PasswordUpdateForm(forms.ModelForm):
+    old_password = forms.CharField(
+        widget=forms.PasswordInput(
+            attrs={
+                "class": "form-control",
+                "placeholder": "Old password",
+            }
+        )
+    )
+    new_password = forms.CharField(
+        widget=forms.PasswordInput(
+            attrs={
+                "class": "form-control",
+                "placeholder": "New password",
+                "onkeyup": "passwordValidator()",
+            }
+        )
+    )
+    new_password2 = forms.CharField(
+        widget=forms.PasswordInput(
+            attrs={
+                "class": "form-control",
+                "placeholder": "repeat new password",
+                "onkeyup": "passwordValidator()",
+            }
+        )
+    )
+
+    class Meta:
+        model = User
+        fields = []
+        labels = {"new_password2":"repeat new password"}
+
+    def clean(self, *args, **kwargs):
+        user = self.user
+        old_password = self.cleaned_data.get("old_password")
+        new_password = self.cleaned_data.get("new_password")
+        new_password2 = self.cleaned_data.get("new_password2")
+        if user.check_password(old_password):
+            if new_password == new_password2:
+                try:
+                    # If the password is valid, return None. If the password is invalid, raise ValidationError with all error messages.
+                    password_validation.validate_password(new_password, user)
+                    return super(PasswordUpdateForm, self).clean(*args, **kwargs)
+                except ValidationError as err:
+                    raise forms.ValidationError(err.messages)
+            else:
+                raise forms.ValidationError("The two new password fields didn’t match.")
+        else:
+            raise forms.ValidationError("The old password is incorrect.")
--- django101Project/views.py
+++ django101Project/views.py
@@ -4,9 +4,14 @@ from django.http import Http404
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import TemplateView
 
+from django.contrib.auth.decorators import login_required
+
+from django.contrib.auth import login, authenticate
+
+
 from django.contrib import messages
 
-from .forms import UserUpdateForm, ProfileUpdateForm
+from .forms import UserUpdateForm, ProfileUpdateForm, PasswordUpdateForm
 
 get_path_name = lambda request: request.path.strip("/") or "/"
 
@@ -44,10 +49,12 @@ def handler404(request, exception):
 class Settings(LoginRequiredMixin, TemplateView):
     template_name = "user/user-settings.html"
 
-    def get(self, request):
+    def get(self, request, *args, **kwargs):
         self.user_form = UserUpdateForm(instance=request.user)
         self.profile_form = ProfileUpdateForm(instance=request.user.profile)
-        return super().render_to_response(self.get_context_data())
+        self.password_form = PasswordUpdateForm()
+        self.form = None
+        return super(Settings, self).get(request, *args, **kwargs)
 
     def is_changed(self, user, instance):
         username_is_changed = user.username != instance.user.username
@@ -57,34 +64,52 @@ class Settings(LoginRequiredMixin, TemplateView):
         print(locals().values())
         return any(filter(lambda vlaue: type(vlaue) is bool, locals().values()))
 
-    def post(self, request):
+    def post(self, request, *args, **kwargs):
         self.user_form = UserUpdateForm(request.POST, instance=request.user)
+        self.password_form = PasswordUpdateForm(request.POST)
+        self.form = self.request.GET.get("form")
         self.profile_form = ProfileUpdateForm(
             request.POST, request.FILES, instance=request.user.profile
         )
-
-        if self.profile_form.is_valid() and self.user_form.is_valid():
-            instance = self.profile_form.save(commit=False)
-            if (
-                user := User.objects.filter(email=instance.user.email).first()
-            ) and user.id != instance.user.id:
-                messages.error(request, "Email is taken.")
-            else:
-                if self.is_changed(user, instance):
-                    instance.save()
-                    self.user_form.save()
-                    messages.success(request, "Profile updated successfully.")
+        if self.form == "update_profile":
+            if self.profile_form.is_valid() and self.user_form.is_valid():
+                instance = self.profile_form.save(commit=False)
+                if (
+                    user := User.objects.filter(email=instance.user.email).first()
+                ) and user.id != instance.user.id:
+                    messages.error(request, "Email is taken.")
                 else:
-                    messages.warning(request, "Nothing be changed.")
-        else:
-            if self.user_form.errors.get("username"):
-                messages.error(request, "Username is taken.")
+                    if self.is_changed(user, instance):
+                        instance.save()
+                        self.user_form.save()
+                        messages.success(request, "Profile updated successfully.")
+                    else:
+                        messages.warning(request, "Nothing be changed.")
             else:
-                messages.error(request, "Profile update unsuccessful.")
+                if user_error := self.user_form.errors.get("username"):
+                    messages.error(request, user_error[0])
+                else:
+                    messages.error(request, "Profile update unsuccessful.")
+        elif self.form == "update_password":
+            user = request.user
+            self.password_form.user = user
+            if self.password_form.is_valid():
+                new_password = self.password_form.cleaned_data.get("new_password")
+                user.set_password(new_password)
+                user.save()
+                user_authenticated = authenticate(
+                    username=user.username, password=new_password
+                )
+                login(request, user_authenticated)
+                messages.success(request, "Password changed successfully.")
+            else:
+                messages.error(request, "Password was not changed successfully.")
+                return super(Settings, self).get(request, *args, **kwargs)
         return redirect("/settings/")
 
     def get_context_data(self, **kwargs):
         context = super(Settings, self).get_context_data(**kwargs)
         context.update(**kwargs)
-        context["forms"] = self.user_form, self.profile_form
+        context["profile_forms"] = self.user_form, self.profile_form
+        context["password_form"] = self.password_form
         return context
